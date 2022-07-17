7fb7d43fea0effc0489ff2edc2a39611edf451e3
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-07-17 16:53:38 +0530

    Added constant messages for displaying messages

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 const RULES_FILENAME: string = 'rules.yml';
 
-const messages: {[key: string]: string} = {
-	"rulesFileMissing": `${RULES_FILENAME} is not present in the working directory`,
-	"folderNotLaunched": 'Please launch vscode with a folder'
+const messages: { [key: string]: string } = {
+	rulesFileMissing: `${RULES_FILENAME} is not present in the working directory`,
+	folderNotLaunched: 'Please launch vscode with a folder',
 };
 
-export {RULES_FILENAME, messages};
+export { RULES_FILENAME, messages };
