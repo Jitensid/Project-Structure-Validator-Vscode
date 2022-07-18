caa8a2b643cc937b92c50a79005e670625d78b78
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-07-18 23:41:26 +0530

    Extracting and validation of Rules complete

--- src/extension.ts
+++ src/extension.ts
@@ -1,76 +1,97 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as fs from 'fs';
-import * as path from 'path';
-import * as vscode from 'vscode';
-import * as YAML from 'yaml';
-import { messages } from './constants';
-import Rules from './interfaces/RulesInterface';
+import betterAjvErrors from "better-ajv-errors";
+import * as path from "path";
+import * as vscode from "vscode";
+import { messages, RULES_FILENAME } from "./constants";
+import { rulesSchema, validateRulesSchema } from "./schema/RulesSchema";
 import {
-	checkIfFolderIsLaunched,
-	checkRulesFilesExistingOrNot,
-	cleanUpExistingFileSystemWatchers,
-	setupFileSystemWatcher,
-} from './utils';
+  checkIfFolderIsLaunched,
+  checkRulesFilesExistingOrNot,
+  cleanUpExistingFileSystemWatchers,
+  loadDataFromRulesFile,
+  setupFileSystemWatcher,
+} from "./utils";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
 
-	// array to store filesystem watchers for different files
-	let watchers: vscode.FileSystemWatcher[] = [];
+  // array to store filesystem watchers for different files
+  let watchers: vscode.FileSystemWatcher[] = [];
 
-	let validateProjectStructure = vscode.commands.registerCommand(
-		'project-structure-validator.validateProjectStructure',
-		() => {
-			// if vscode is not launched with a folder then raise an error message
-			if (!checkIfFolderIsLaunched()) {
-				vscode.window.showErrorMessage(messages.folderNotLaunched);
-				return;
-			}
+  let validateProjectStructure = vscode.commands.registerCommand(
+    "project-structure-validator.validateProjectStructure",
+    () => {
+      // if vscode is not launched with a folder then raise an error message
+      if (!checkIfFolderIsLaunched()) {
+        vscode.window.showErrorMessage(messages.folderNotLaunched);
+        return;
+      }
 
-			// if rules file is absent in the folder then raise an error message
-			if (!checkRulesFilesExistingOrNot()) {
-				vscode.window.showErrorMessage(messages.rulesFileMissing);
-				return;
-			}
+      // if rules file is absent in the folder then raise an error message
+      if (!checkRulesFilesExistingOrNot()) {
+        vscode.window.showErrorMessage(messages.rulesFileMissing);
+        return;
+      }
 
-			// clean up existing filesystem watchers
-			watchers = cleanUpExistingFileSystemWatchers(watchers);
+      // path to the rules file present in the folder
+      const rulesFilePath = path.join(
+        vscode.workspace.workspaceFolders?.[0].uri.fsPath!,
+        RULES_FILENAME
+      );
 
-			watchers.push(setupFileSystemWatcher());
+      // extract the rulesData from the rules file present in the directory
+      const rulesData: any = loadDataFromRulesFile(rulesFilePath);
 
-			for (let watcher of watchers) {
-				watcher.onDidCreate((event) => {
-					vscode.window.showInformationMessage(event.fsPath);
-				});
-			}
+      // evaluate the RulesSchema present in the rules file given by the user
+      const valid = validateRulesSchema(rulesData);
 
-			const x: string = fs.readFileSync(
-				path.join(
-					vscode.workspace.workspaceFolders?.[0].uri.fsPath!,
-					'rules.yml'
-				),
-				'utf-8'
-			);
+      // if RulesSchema validation fails
+      if (!valid) {
+        const output = betterAjvErrors(
+          rulesSchema,
+          rulesData,
+          validateRulesSchema.errors!,
+          { format: "js" }
+        );
 
-			const rulesData: any = YAML.parse(x);
+        // display appropriate error message if there is a syntax error
+        // present in the rules file
+        vscode.window.showErrorMessage(messages.rulesFilesHasSyntaxError);
 
-			console.log(rulesData);
+        return;
+      }
 
-			// display a confirmation message to the user that the command execution is successful
-			vscode.window.showInformationMessage(
-				'FileSystem Validator is Ready!'
-			);
-		}
-	);
+      // clean up existing filesystem watchers
+      watchers = cleanUpExistingFileSystemWatchers(watchers);
 
-	context.subscriptions.push(validateProjectStructure);
+      // append watchers based on existing rules present in rules.yml file
+      watchers.push(setupFileSystemWatcher());
+
+      // iterate all filesystem watchers and attach a onChange event
+      for (let watcher of watchers) {
+        watcher.onDidCreate((event) => {
+          vscode.window.showInformationMessage(event.fsPath);
+        });
+      }
+
+      vscode.window.showInformationMessage("Worked");
+    }
+  );
+
+  let a = vscode.commands.registerCommand(
+    "project-structure-validator.a",
+    () => {}
+  );
+
+  context.subscriptions.push(validateProjectStructure);
+  context.subscriptions.push(a);
 }
 
 // this method is called when your extension is deactivated
--- src/utils.ts
+++ src/utils.ts
@@ -1,85 +1,95 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-import { RULES_FILENAME } from './constants';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as YAML from "yaml";
+import { RULES_FILENAME } from "./constants";
 
-const readFile = (filePath: string) => {
-	const file: string = fs.readFileSync(filePath, 'utf-8');
-	return file;
+/**
+ * Function to read the data present in a file
+ * @param {string} filePath - string value of the path of the file
+ * @returns {string} file - contents of the file
+ */
+const loadDataFromRulesFile = (filePath: string): string => {
+  // read data of file in string
+  const fileContents: string = fs.readFileSync(filePath, "utf-8");
+
+  // parse yaml data as string
+  const rulesData: any = YAML.parse(fileContents);
+
+  // return the data extracted from the file
+  return rulesData;
 };
 
-const setupFileSystemWatcher = (programmingLanguage: string) => {
-	let watcher: any = null;
+const setupFileSystemWatcher = (): vscode.FileSystemWatcher => {
+  let watcher: vscode.FileSystemWatcher;
 
-	if (programmingLanguage === 'Python') {
-		watcher = vscode.workspace.createFileSystemWatcher(
-			new vscode.RelativePattern(
-				vscode.workspace.workspaceFolders?.[0]!,
-				'**/*.py'
-			),
-			false,
-			true,
-			true
-		);
-	} else if (programmingLanguage === 'Javascript') {
-		watcher = vscode.workspace.createFileSystemWatcher(
-			new vscode.RelativePattern(
-				vscode.workspace.workspaceFolders?.[0]!,
-				'**/*.js'
-			),
-			false,
-			true,
-			true
-		);
-	}
+  watcher = vscode.workspace.createFileSystemWatcher(
+    new vscode.RelativePattern(
+      vscode.workspace.workspaceFolders?.[0]!,
+      "**/*.{png,jpg,gif,jpeg,mp3,mp4}"
+    ),
+    false,
+    true,
+    true
+  );
 
-	return watcher;
+  return watcher;
 };
 
-// function to check if vscode is launched with a folder or not
+/**
+ * Function to check if vscode is launched with a folder or not
+ * @returns {boolean} - true if vscode is launched with a folder or else return false
+ */
 const checkIfFolderIsLaunched = (): boolean => {
-	// if no workspace folder found then return false
-	if (vscode.workspace.workspaceFolders === undefined) {
-		return false;
-	}
-	return true;
+  // if no workspace folder found then return false
+  if (vscode.workspace.workspaceFolders === undefined) {
+    return false;
+  }
+  return true;
 };
 
-// function to check if the folder launched with vscode has the required rules file
+/**
+ * Function to check if the folder launched with vscode has the required rules file
+ * @returns {boolean} - true if rules file is present else return false
+ */
 const checkRulesFilesExistingOrNot = (): boolean => {
-	// set the path og the file containing rules to check if it exists
-	const rulesFilePath = path.join(
-		vscode.workspace.workspaceFolders?.[0]?.uri?.fsPath!,
-		RULES_FILENAME
-	);
+  // set the path og the file containing rules to check if it exists
+  const rulesFilePath = path.join(
+    vscode.workspace.workspaceFolders?.[0]?.uri?.fsPath!,
+    RULES_FILENAME
+  );
 
-	// return true if present 
-	if (fs.existsSync(rulesFilePath)) {
-		return true;
-	}
+  // return true if present
+  if (fs.existsSync(rulesFilePath)) {
+    return true;
+  }
 
-	return false;
+  return false;
 };
 
-// function to dispose the existing file system watchers
+/**
+ * Function to dispose the existing file system watchers
+ * @param {vscode.FileSystemWatcher[]} watchers - array of filesystem watchers
+ * @returns {vscode.FileSystemWatcher[]} watchers - array of filesystem watchers
+ */
 const cleanUpExistingFileSystemWatchers = (
-	watchers: vscode.FileSystemWatcher[]
+  watchers: vscode.FileSystemWatcher[]
 ): vscode.FileSystemWatcher[] => {
-	//  dispose previously existing FileSystemWatcher
-	for (let watcher of watchers) {
-		watcher.dispose();
-	}
+  //  dispose previously existing FileSystemWatcher
+  for (let watcher of watchers) {
+    watcher.dispose();
+  }
 
-	// remove all the elements from the watchers array
-	watchers.splice(0);
+  // remove all the elements from the watchers array
+  watchers.splice(0);
 
-	return watchers;
+  return watchers;
 };
 
 export {
-	readFile,
-	setupFileSystemWatcher,
-	cleanUpExistingFileSystemWatchers,
-	checkRulesFilesExistingOrNot,
-	checkIfFolderIsLaunched
+  loadDataFromRulesFile,
+  setupFileSystemWatcher,
+  cleanUpExistingFileSystemWatchers,
+  checkRulesFilesExistingOrNot,
+  checkIfFolderIsLaunched,
 };
