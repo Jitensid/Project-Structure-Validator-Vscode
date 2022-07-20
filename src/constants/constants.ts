05ea500660c09b495b4a2c780d380a33f25bb9f1
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-07-20 14:27:56 +0530

    Added Prettier Config for git hooks

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
-const RULES_FILENAME: string = "rules.yml";
+const RULES_FILENAME: string = 'rules.yml';
 
 const messages: { [key: string]: string } = {
-  rulesFileMissing: `${RULES_FILENAME} is not present in the working directory`,
-  folderNotLaunched: "Please launch vscode with a folder",
-  rulesFilesHasSyntaxError: `${RULES_FILENAME} has a syntax error`,
+    rulesFileMissing: `${RULES_FILENAME} is not present in the working directory`,
+    folderNotLaunched: 'Please launch vscode with a folder',
+    rulesFilesHasSyntaxError: `${RULES_FILENAME} has a syntax error`,
 };
 
 export { RULES_FILENAME, messages };
