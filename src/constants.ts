24fdef5087aa4f6b5dc64e98059140f31b0305a0
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-07-20 12:34:11 +0530

    deleted constants.ts from src folder

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-const RULES_FILENAME: string = "rules.yml";
-
-const messages: { [key: string]: string } = {
-  rulesFileMissing: `${RULES_FILENAME} is not present in the working directory`,
-  folderNotLaunched: "Please launch vscode with a folder",
-  rulesFilesHasSyntaxError: `${RULES_FILENAME} has a syntax error`,
-};
-
-export { RULES_FILENAME, messages };
