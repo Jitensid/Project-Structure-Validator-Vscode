2e228a0c8b888b3490c506faaee3882e67ddec66
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-07-21 14:56:17 +0530

    Removed unnecessary constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,12 +1,12 @@
-const RULES_FILENAME: string = 'rules.yml';
-
+// name of the config file or the property that will be used by cosmiConfig
 const COSMIC_CONFIG_MODULE_NAME: string = 'structure';
 
+// dictionary to store relevant messages
 const messages: { [key: string]: string } = {
-    configFileMissing: `${RULES_FILENAME} is not present in the working directory`,
+    configFileMissing: 'No config found',
     folderNotLaunched: 'Please launch vscode with a folder',
     rulesFilesHasSyntaxError: 'Syntax Error while parsing the config file',
     configFileEmptyOrMissing: 'Config File is missing or it is empty',
 };
 
-export { RULES_FILENAME, messages, COSMIC_CONFIG_MODULE_NAME };
+export { messages, COSMIC_CONFIG_MODULE_NAME };
