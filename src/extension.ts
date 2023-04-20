779310d9c254fa5704f61e5ffc9066bf25db69f6
GitHub Action <actions@github.com>
2023-04-20 21:30:57 +0000

    Auto-format code using Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -59,15 +59,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
-
-
-
-
-
-
-
-
-
-
-
-
